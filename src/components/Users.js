fd59a8de2f0773dd4f14e96da68f6f0c24b0fece
Pedro Rorato <pedroberorato@gmail.com>
2021-12-13 09:20:51 -0300

    convert functional components to class-based

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,37 +1,45 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import User from './User';
 
+//Styles
 import classes from './Users.module.css';
 
+//Data
 const DUMMY_USERS = [
   { id: 'u1', name: 'Max' },
   { id: 'u2', name: 'Manuel' },
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
-
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
+class Users extends Component {
+  state = {
+    showUsers: true
   };
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+  toggleUsersHandler() {
+    this.setState((currentState) => {
+      return { showUsers: !currentState.showUsers }
+    })
+  };
 
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
-};
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
 
 export default Users;
